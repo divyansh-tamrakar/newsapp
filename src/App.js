cd51392c86b0c031fec75e34418a69c85403830f
divyansh-tamrakar <divyansh.tamrakar17@gmail.com>
2023-09-20 01:04:35 +0530

    included Routers

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
-
-// API_key for newsapi.org = 221654f1c2424239af9c0f379e3b6076
-
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+import { BrowserRouter as Router, Route, NavLink, Routes} from 'react-router-dom'
+
+// API_key for newsapi.org = 221654f1c2424239af9c0f379e3b6076
 
 export default class App extends Component {
     render() {
         return (
             <div>
                 <Navbar />
-                <News pageSize={5} country='in' category='sports'/>
+                <Router>
+                    <Routes>
+                        <Route exact path='/' element={<News pageSize={5} country='in' category='sports'/>} />
+                    </Routes>
+                </Router>
             </div>
         )
-  }
+    }
 }
